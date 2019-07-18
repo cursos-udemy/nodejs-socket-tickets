fb7cd1a0522b2a6f8766b9d9f1fa2727b9acfa09
gwfernandez <gwfernandez@gmail.com>
2019-07-17 22:56:13 -0300

    lista de tickets pendientes

--- server/classes/TicketControl.js
+++ server/classes/TicketControl.js
@@ -1,14 +1,29 @@
 const fs = require('fs');
 
+class Ticket {
+    constructor(ticketID, box, user) {
+        this.timestamp = new Date();
+        this.status = 'PENDING';
+        this.ticketID = ticketID;
+        this.box = box;
+        this.user = user;
+    }
+}
 class TicketControl {
     constructor() {
         this.lastTicket = 0;
         this.attentionDate = new Date();
+        this.tickets = [];
     }
 
     saveControl() {
         try {
-            const data = new Uint8Array(Buffer.from(JSON.stringify({ lastTicket: this.lastTicket, attentionDate: this.attentionDate })));
+            const status = {
+                lastTicket: this.lastTicket,
+                attentionDate: this.attentionDate,
+                tickets: this.tickets
+            };
+            const data = new Uint8Array(Buffer.from(JSON.stringify(status)));
             fs.writeFileSync('./server/data/control.json', data);
         } catch (error) {
             console.error('Error al persistir la informacion de control');
@@ -19,6 +34,7 @@ class TicketControl {
     reinitialize() {
         this.lastTicket = 0;
         this.attentionDate = new Date();
+        this.tickets = [];
         this.saveControl();
         console.info('reinitilize [OK]');
     }
@@ -27,8 +43,10 @@ class TicketControl {
         let data = require('../data/control.json');
         if (data) {
             this.lastTicket = data.lastTicket || 0;
+            this.tickets = data.tickets || [];
         } else {
             this.lastTicket = 0;
+            this.tickets = [];
         }
         this.attentionDate = new Date();
         this.saveControl();
@@ -41,20 +59,16 @@ class TicketControl {
 
     nextTicket(request) {
         this.lastTicket += 1;
+        const ticket = new Ticket(this.lastTicket, request.box, request.user);
+        this.tickets.push(ticket);
         this.saveControl();
         console.info('execute nextTicket: [OK]');
-        const ticket = {
-            ticketID: this.lastTicket,
-            userAssigned: request.user,
-            timestamp: new Date().getTime(),
-            box: request.box
-        };
-        return ticket;
+        return {...ticket};
     }
 
     getStatus() {
         return {
-           ticketID: this.lastTicket 
+            ticketID: this.lastTicket
         }
     }
 
